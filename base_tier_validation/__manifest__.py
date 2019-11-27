73ca55c9dfdcf00f05730422ca77c5ac5a7f9f46
Pedro Gonzalez <pedro.gonzalez@pesol.es>
2019-11-27 13:07:22 +0100

    [IMP] base_tier_validation: black, isort

--- base_tier_validation/__manifest__.py
+++ base_tier_validation/__manifest__.py
@@ -5,25 +5,19 @@
     "summary": "Implement a validation process based on tiers.",
     "version": "12.0.3.2.1",
     "development_status": "Mature",
-    "maintainers": ['lreficent'],
+    "maintainers": ["lreficent"],
     "category": "Tools",
     "website": "https://github.com/OCA/server-ux",
     "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "web",
-        "bus",
-    ],
+    "depends": ["web", "bus"],
     "data": [
         "security/ir.model.access.csv",
         "views/tier_definition_view.xml",
         "views/tier_review_view.xml",
         "views/assets_backend.xml",
     ],
-    'qweb': [
-        'static/src/xml/systray.xml',
-        'static/src/xml/tier_review_template.xml',
-    ],
+    "qweb": ["static/src/xml/systray.xml", "static/src/xml/tier_review_template.xml"],
 }
